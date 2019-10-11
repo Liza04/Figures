84406029175f30fc0b4add4f2e203f35548e7083
Liza04 <55574567+Liza04@users.noreply.github.com>
2019-10-11 13:37:18 +0700

    Update mane.py

--- mane.py
+++ mane.py
@@ -1,2 +1,23 @@
 #Case1
 Import tirtle
+def square(x,y,size,colour,angle)
+ #TODO:(Anya) 
+  pass
+def trangle(x,y,size,colour,angle)
+ #TODO:(Sasha) 
+  pass
+def rhombus(x,y,size,colour,angle)
+ #TODO:(Liza) 
+  pass
+#(Anya) rabbit
+
+#(Sasha) fish
+
+#(Liza) squirrel
+
+#(Anya) human
+
+#(Sasha) human2
+
+#(Liza) rocket
+
