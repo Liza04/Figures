22060504de0b60b59c205f22e584bd420a7bd988
Liza04 <10foxgor04@gmail.com>
2019-10-24 21:59:10 +0700

    Trangle (develop)

--- mane.py
+++ mane.py
@@ -1,19 +1,33 @@
 #Case1
-Import tirtle
-def square(x,y,size,colour,angle)
+
+
+import turtle
+def square(x,y,size,colour,angle):
  #TODO:(Anya) 
   pass
-def trangle(x,y,size,colour,angle)
+def trangle(x,y,size,colour,angle):
  #TODO:(Sasha) 
   pass
-def rhombus(x,y,size,colour,angle)
- #TODO:(Liza) 
-  pass
+def rhombus(x,y,size,degrees,angle,color):
+ #TODO:(Liza)
+ turtle.penup()
+ turtle.goto(x, y)
+ turtle.pendown()
+ turtle.color(color) #Цвет
+ turtle.right(angle)  # повернуть курсор
+ turtle.forward(size)  # задать размер стороны
+ turtle.right(180 - degrees-angle)  # второй угол
+ turtle.forward(size)  # сторона
+ turtle.right(degrees+angle)  # третий угол
+ turtle.forward(size)  # сторона
+ turtle.right(180 - degrees-angle)  # четвертый угол
+ turtle.forward(size)  # последняя сторона
 #(Anya) rabbit
 
 #(Sasha) fish
 
 #(Liza) squirrel
+rhombus(100,100,300,80,30,'green')
 
 #(Anya) human
 
